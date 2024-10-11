3f15f3f4017b291b8ede3eb2336d44edcb212541
kortik425 <jaycee425.develop@gmail.com>
2024-10-12 01:15:42 +0200

    feat: Set basic styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primaryDark: "--primary-dark",
+        secondaryDark: "--secondary-dark",
+        primaryLight: "--primary-light",
+        secondaryLight: "--secondary-light",
       },
     },
   },
