63aff602cd12846d71878bcf688a4ab94e18a3bd
kortik425 <jaycee425.develop@gmail.com>
2024-10-16 00:27:22 +0200

    feat: Add header for search input

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -7,10 +7,11 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 
-import { Card } from "@/components/UI";
+import { Card, TextInput } from "@/components/UI";
 import { useDataContext } from "@/contexts/data";
 import { User } from "@/interfaces/Idata";
 import { TableRow, TableHeader } from "@/components/table";
+import searchIcon from "../../assets/search.svg";
 
 interface UsersProps {
   // define your props here
@@ -52,31 +53,47 @@ const Users: React.FC<UsersProps> = ({}) => {
   };
 
   return (
-    <main className="px-4 pt-6 flex gap-x-12 items-start overflow-hidden">
-      <table>
-        <TableHeader headerGroups={userTable.getHeaderGroups()} />
-        <tbody>
-          {userTable.getRowModel().rows.map((row) => (
-            <TableRow key={row.id} row={row} />
-          ))}
-        </tbody>
-      </table>
-      <div className="h-[100%] flex flex-col flex-1">
-        <h2>Posts</h2>
-        <div className="overflow-y-auto">
-          {posts.length !== 0 &&
-            posts.map((post) => {
-              return (
-                <Card
-                  key={post.id}
-                  title={post.title}
-                  className="mb-5"
-                  onClick={showPost}
-                />
-              );
-            })}
+    <main className="overflow-hidden flex flex-col px-4 pt-6">
+      <header>
+        <TextInput
+          label="Search User"
+          icon={{ img: searchIcon, alt: "search-icon" }}
+          isLabelHidden
+        >
+          <button
+            className="bg-secondaryLight rounded-[6px] px-2 m-1"
+            onClick={() => {}}
+          >
+            Action
+          </button>
+        </TextInput>
+      </header>
+      <section className=" flex gap-x-12 items-start overflow-hidden">
+        <table>
+          <TableHeader headerGroups={userTable.getHeaderGroups()} />
+          <tbody>
+            {userTable.getRowModel().rows.map((row) => (
+              <TableRow key={row.id} row={row} />
+            ))}
+          </tbody>
+        </table>
+        <div className="h-[100%] flex flex-col flex-1">
+          <h2>Posts</h2>
+          <div className="overflow-y-auto">
+            {posts.length !== 0 &&
+              posts.map((post) => {
+                return (
+                  <Card
+                    key={post.id}
+                    title={post.title}
+                    className="mb-5"
+                    onClick={showPost}
+                  />
+                );
+              })}
+          </div>
         </div>
-      </div>
+      </section>
     </main>
   );
 };
