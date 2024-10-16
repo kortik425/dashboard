ed92c374734274b618408a1593d80547af16788c
kortik425 <jaycee425.develop@gmail.com>
2024-10-16 21:22:30 +0200

    feat: Use modal in the user just set up content is still going to be fixed

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -5,13 +5,16 @@ import { Card, TextInput } from "@/components/UI";
 import { useDataContext } from "@/contexts/data";
 
 import searchIcon from "../../assets/search.svg";
-import UserTable from "./user-table";
+import UserTable from "./components/user-table";
+import Modal from "@/components/modal/modal";
+import { useModal } from "@/contexts/modals";
 
 interface UsersProps {}
 
 const Users: React.FC<UsersProps> = ({}) => {
   const [filters, setFilters] = useState("");
-  const { posts } = useDataContext();
+  const { isOpen, closeModal } = useModal();
+  const { posts, user } = useDataContext();
 
   const showPost = (e: React.MouseEvent) => {
     console.log(e);
@@ -22,43 +25,49 @@ const Users: React.FC<UsersProps> = ({}) => {
   };
 
   return (
-    <main className="overflow-hidden flex flex-col px-4 pt-6 ">
-      <header>
-        <TextInput
-          label="Search User"
-          icon={{ img: searchIcon, alt: "search-icon" }}
-          isLabelHidden
-          value={filters}
-          onChange={handleInput}
-        >
-          <button
-            className="bg-secondaryLight rounded-[6px] px-2 m-1"
-            onClick={() => {}}
+    <>
+      <main className="overflow-hidden flex flex-col px-4 pt-6 ">
+        <header>
+          <TextInput
+            label="Search User"
+            icon={{ img: searchIcon, alt: "search-icon" }}
+            isLabelHidden
+            value={filters}
+            onChange={handleInput}
           >
-            Action
-          </button>
-        </TextInput>
-      </header>
-      <section className="flex flex-1 gap-x-12 items-start overflow-hidden">
-        <UserTable filters={filters} />
-        <div className="h-[100%] flex flex-col flex-1">
-          <h2>Posts</h2>
-          <div className="overflow-y-auto">
-            {posts.length !== 0 &&
-              posts.map((post) => {
-                return (
-                  <Card
-                    key={post.id}
-                    title={post.title}
-                    className="mb-5"
-                    onClick={showPost}
-                  />
-                );
-              })}
+            <button
+              className="bg-secondaryLight rounded-[6px] px-2 m-1"
+              onClick={() => {}}
+            >
+              Action
+            </button>
+          </TextInput>
+        </header>
+        <section className="flex flex-1 gap-x-12 items-start overflow-hidden">
+          <UserTable filters={filters} />
+          <div className="h-[100%] flex flex-col flex-1">
+            <h2>Posts</h2>
+            <div className="overflow-y-auto">
+              {posts.length !== 0 &&
+                posts.map((post) => {
+                  return (
+                    <Card
+                      key={post.id}
+                      title={post.title}
+                      className="mb-5"
+                      onClick={showPost}
+                    />
+                  );
+                })}
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+      <Modal title="lorem" isModalOpen={isOpen} abortFn={closeModal}>
+        <p className="mt-2 text-sm text-gray-500">fsdfdsfsdfd</p>
+        <p>{user?.email}</p>
+      </Modal>
+    </>
   );
 };
 
