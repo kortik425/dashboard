bdc7252bfb01c83d7c9316d87447c01a9b521d9e
kortik425 <jaycee425.develop@gmail.com>
2024-10-17 02:31:10 +0200

    fix: Use Camelcase for svg properties They are now React components

--- src/components/UI/icons/overview.tsx
+++ src/components/UI/icons/overview.tsx
@@ -10,12 +10,12 @@ const OverviewIcon = ({}) => {
       viewBox="0 0 512 512"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
 
       <g id="SVGRepo_iconCarrier">
--- src/components/UI/icons/trash.tsx
+++ src/components/UI/icons/trash.tsx
@@ -10,14 +10,14 @@ const IdCardIcon = ({}) => {
       viewBox="-32 0 512 512"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+      <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         stroke="#CCCCCC"
-        stroke-width="3.072"
+        strokeWidth="3.072"
       />
 
       <g id="SVGRepo_iconCarrier">
