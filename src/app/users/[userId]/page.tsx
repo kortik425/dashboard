96dbcdb1ddbf6bdc1868dcd8f5952a4a858edcc7
kortik425 <jaycee425.develop@gmail.com>
2024-10-17 12:48:11 +0200

    feat: Add dynamic route for user

---
+++ src/app/users/[userId]/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import UserContent from "../components/user-content";
+import { User } from "@/interfaces/Idata";
+
+interface UserProps {
+  params: { userId: string };
+}
+
+const UserDetail: React.FC<UserProps> = async ({ params }) => {
+  const { userId } = params;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+  const user: User = await response.json();
+
+  return (
+    <div className="m-auto">
+      <h1>Informations of: {user.name}</h1>
+      <UserContent user={user} />
+    </div>
+  );
+};
+
+export default UserDetail;
